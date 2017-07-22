92f0f69cb596e25e1b5c04eb4a503f9e44fbbc04
John Bindel <johnbindel@gmail.com>
2017-07-22 10:32:26 -0700

    Committed initial version of AzurePing

---
+++ src/AzurePing/AzurePing/Ping.cs
@@ -0,0 +1,19 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace AzurePing
+{
+    public static class Ping
+    {
+        [FunctionName("Ping")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            return req.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
